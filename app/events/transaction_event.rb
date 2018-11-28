dleve123/exchange

commit b405d31909409788711515a56bcdb8daa42d5748

add total_list_price_cents to order events

app/events/transaction_event.rb
@@ -29,6 +29,7 @@ class TransactionEvent < Events::BaseEvent
         currency_code: order.currency_code,
         fulfillment_type: order.fulfillment_type,
         items_total_cents: order.items_total_cents,
+        total_list_price_cents: order.total_list_price_cents,
         line_items: order.line_items.map { |li| line_item_detail(li) },
         seller_id: order.seller_id,
         seller_type: order.seller_type,

commit 31ae297cf3faae48259ca1bcc3a6943d6640ce7b

Remove total_list_price_cents from TransactionEvent
* This diff was wrongly introduced in
b405d31909409788711515a56bcdb8daa42d5748
* It was intended to include total_list_price_cents in the OrderEvent,
not the TransactionEvent.
  + This is evident from the [PR description introducing this change][0]

[0]: https://github.com/artsy/exchange/pull/287

app/events/transaction_event.rb
@@ -29,7 +29,6 @@ class TransactionEvent < Events::BaseEvent
         currency_code: order.currency_code,
         fulfillment_type: order.fulfillment_type,
         items_total_cents: order.items_total_cents,
-        total_list_price_cents: order.total_list_price_cents,
         line_items: order.line_items.map { |li| line_item_detail(li) },
         seller_id: order.seller_id,
         seller_type: order.seller_type,

spec/events/transaction_event_spec.rb
@@ -88,7 +88,6 @@ describe TransactionEvent, type: :events do
       expect(event.properties[:order][:seller_id]).to eq partner_id
       expect(event.properties[:order][:seller_type]).to eq 'gallery'
       expect(event.properties[:order][:items_total_cents]).to eq 300
-      expect(event.properties[:order][:total_list_price_cents]).to eq 400
       expect(event.properties[:order][:updated_at]).not_to be_nil
       expect(event.properties[:order][:created_at]).not_to be_nil
       expect(event.properties[:order][:line_items].count).to eq 2